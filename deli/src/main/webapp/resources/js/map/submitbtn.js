c73ea7c4b2db4fb7bb27239721f768cc4e0f03f2
Kohaneul <96707563+Kohaneul@users.noreply.github.com>
2022-07-26 14:52:22 +0900

    0726 haneul commit

---
+++ deli/src/main/webapp/resources/js/map/submitbtn.js
@@ -0,0 +1,7 @@
+let submitbtn= document.getElementById('submitbtn');
+submitbtn.addEventListener('mouseover',function(){
+	submitbtn.style.backgroundColor="#48D1CC"});
+
+submitbtn.addEventListener('mouseout',function(){
+submitbtn.style.backgroundColor="black";
+});
