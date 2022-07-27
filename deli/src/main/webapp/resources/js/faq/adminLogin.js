e2a9e5b4bfd5d1bfdabd06fdc17eee07bf7bff23
Kohaneul <96707563+Kohaneul@users.noreply.github.com>
2022-07-27 15:33:13 +0900

    0727 haneul commit

---
+++ deli/src/main/webapp/resources/js/faq/adminLogin.js
@@ -0,0 +1,15 @@
+let submitbtn= document.getElementById('btn1');
+submitbtn.addEventListener('mouseover',function(){
+	submitbtn.style.backgroundColor="#48D1CC"});
+
+submitbtn.addEventListener('mouseout',function(){
+submitbtn.style.backgroundColor="black";
+});
+
+let submitbtn2= document.getElementById('btn2');
+submitbtn2.addEventListener('mouseover',function(){
+	submitbtn2.style.backgroundColor="#48D1CC"});
+
+submitbtn2.addEventListener('mouseout',function(){
+submitbtn2.style.backgroundColor="black";
+});
