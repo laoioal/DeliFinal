d4c08029ab8af65ba52da0a1bcf6872a122182d5
Kohaneul <96707563+Kohaneul@users.noreply.github.com>
2022-07-28 12:36:09 +0900

    0728 commi

--- deli/src/main/webapp/resources/js/map/restaurantChoose.js
+++ deli/src/main/webapp/resources/js/map/restaurantChoose.js
@@ -1,5 +1,16 @@
-let resno;
-function radioClick(restno){
-	resno = restno;
-	document.getElementsById('no').value = restno;
+function radioClick(no){
+	document.getElementById('no').value = no;
+}
+
+
+
+function onClick(){
+
+	let no = document.getElementById('no').value;
+	let place_name = document.getElementById('place_name2').value;
+	let code = document.getElementById('code').value;
+	if(!no){
+			alert("다시입력해주세요");
+			}
+						
 }
